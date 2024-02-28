30819424762efc74c234c8a391ac00b4f5326351
Rafa16vieira <rafaelvieiraalbu@gmail.com>
2024-02-28 14:00:22 -0300

    add0.3

--- src/navigation/favoritos.tsx
+++ src/navigation/favoritos.tsx
@@ -1,6 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { FavoritosScreen } from '../screens/favoritos';
-import { App } from '../screens/loja';
+//import { LojaScreen } from '../screens/loja';
 
 const Stack = createStackNavigator();
 
@@ -8,7 +8,6 @@ export function FavoritosNavegacao() {
     return (
         <Stack.Navigator>
             <Stack.Screen name="favoritos" component={FavoritosScreen} />
-            <Stack.Screen name="Loja" component={App} />
         </Stack.Navigator>
     )
 }
